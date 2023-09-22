4ad8a8e5f8eacb83982346285f6ba54c60bdf495
Eliza Pant <108278876+panteliza@users.noreply.github.com>
2023-09-22 17:14:03 +0545

    Update Featured.jsx

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -1,12 +1,10 @@
 import useFetch from "../../hooks/useFetch";
 import "./featured.css";
 
-
 const Featured = () => {
   const { data, loading, error } = useFetch(
     "/hotels/countByCity?cities=pokhara,kathmandu,chitwan"
   );
-  
 
   return (
     <div className="featured">
@@ -16,36 +14,36 @@ const Featured = () => {
         <>
           <div className="featuredItem">
             <img
-              src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+              src="https://images.unsplash.com/photo-1576948187290-457c015b3bff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
               alt=""
               className="featuredImg"
             />
             <div className="featuredTitles">
-              <h1 > Pokhara</h1>
-              <h2>{data[0]} properties</h2>
+              <h2>Pokhara</h2>
+              <h3>{data[0]} properties</h3>
             </div>
           </div>
 
           <div className="featuredItem">
             <img
-              src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o="
+              src="https://images.unsplash.com/photo-1529733905113-027ed85d7e33?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80"
               alt=""
               className="featuredImg"
             />
             <div className="featuredTitles">
-              <h1>Kathmandu</h1>
-              <h2>{data[1]} properties</h2>
+              <h2>Kathmandu</h2>
+              <h3>{data[1]} properties</h3>
             </div>
           </div>
           <div className="featuredItem">
             <img
-              src="https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o="
+              src="https://images.unsplash.com/photo-1633483114513-510f15270dd7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
               alt=""
               className="featuredImg"
             />
             <div className="featuredTitles">
-              <h1>Chitwan</h1>
-              <h2>{data[2]} properties</h2>
+              <h2>Chitwan</h2>
+              <h3>{data[2]} properties</h3>
             </div>
           </div>
         </>
